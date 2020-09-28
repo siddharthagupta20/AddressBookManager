3462b386a9f1f26d2561d06c3265ab0bc28642f5
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-09-28 19:52:14 +0530

    [SID] modified: add contact feature introduced

--- src/com/cg/ab/AddressBook.java
+++ src/com/cg/ab/AddressBook.java
@@ -1,5 +1,45 @@
 package com.cg.ab;
 
-public class AddressBook {
+import java.util.*;
 
+
+public class AddressBook {
+	private List<Contact> addressBook = new ArrayList<Contact>();
+
+	public void setAddressBook(List<Contact> m) {
+		this.addressBook = m;
+	}
+
+	public List<Contact> getAddressBook() {
+		return this.addressBook;
+	}
+
+	public void addContact(Contact contactObj) {
+		addressBook.add(contactObj);
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		AddressBook ab = new AddressBook();
+		System.out.println("Welcome to Address Book");
+		System.out.println("Enter first name: ");
+		String firstName = sc.nextLine();
+		System.out.println("Enter last name: ");
+		String lastName = sc.nextLine();
+		System.out.println("Enter address: ");
+		String address = sc.nextLine();
+		System.out.println("Enter city: ");
+		String city = sc.nextLine();
+		System.out.println("Enter state: ");
+		String state = sc.nextLine();
+		System.out.println("Enter zip: ");
+		long zip = sc.nextLong();
+		System.out.println("Enter phone number: ");
+		sc.nextLine();
+		String phoneNumber = sc.nextLine();
+		System.out.println("Enter email: ");
+		String email = sc.nextLine();
+		Contact x = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+		ab.addContact(x);
+		sc.close();
+	}
 }
